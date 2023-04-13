af316dac26201911abb14d44792841b8beb75a4c
jindding <pjm2476@ajou.ac.kr>
2023-04-13 18:42:31 +0900

    1. backend 서버 init 2. database 연결

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mysql = require('mysql');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '1234',
+    database : 'FitMe'
+  })
+
+  connection.connect();
+
+  connection.query('SELECT * from Users', (error, rows, fields) => {
+    if (error) throw error;
+    console.log('User info is: ', rows);
+  });
+  
+  connection.end();
+
+module.exports = app;
