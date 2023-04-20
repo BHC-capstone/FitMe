2a8e3c2a551507b5a202dc9376ac507d6d1d4b96
김재현 <kjh@kmac.local>
2023-04-21 00:00:34 +0900

    signup page commit

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "airbnb",
+    // {"prettier/react",}
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "import/np-resolved": "off",
+    "import/no-extraneous-dependencies": "off",
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "jsx-a11y/label-has-associated-control": [
+      2,
+      {
+        labelAttributes: ["htmlFor"],
+      },
+    ],
+  },
+};
