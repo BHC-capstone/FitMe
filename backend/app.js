3e0824fe80157aa69c1b6bce95430040e599a734
jaehyunk17 <kjh77k@ajou.ac.kr>
2023-04-26 21:57:25 +0900

    cors() 추가

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql');
-
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,25 +16,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/trainers', trainersRouter);
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '1234',
-    database : 'FitMe'
-  })
+// const connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '1234',
+//     database : 'FitMe'
+//   })
 
-  connection.connect();
+//   connection.connect();
 
-  connection.query('SELECT * from Users', (error, rows, fields) => {
-    if (error) throw error;
-    console.log('User info is: ', rows);
-  });
+//   connection.query('SELECT * from Users', (error, rows, fields) => {
+//     if (error) throw error;
+//     console.log('User info is: ', rows);
+//   });
   
-  connection.end();
+//   connection.end();
 
 module.exports = app;
