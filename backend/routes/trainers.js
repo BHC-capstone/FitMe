7578d13a9afa1c746ed3d65646328f85e420dfb4
jindding <pjm2476@ajou.ac.kr>
2023-04-27 20:36:07 +0900

    1. aws s3 - backend 연결 2. 트레이너 회원가입 자격증 첨부 가능하도록 기능 구현

--- backend/routes/trainers.js
+++ backend/routes/trainers.js
@@ -1,150 +1,211 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { trainers } = require('../models');
+const { trainers } = require("../models");
+const multerS3 = require("multer-s3");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const AWS = require("aws-sdk");
 
-
-// trainer signup
-router.post('/signup', async function (req, res) {
-  if (
-    (req.body.username,
-    req.body.password,
-    req.body.password2,
-    req.body.email,
-    req.body.age,
-    req.body.gender)
-  ) {
-    const trainerInfo = await trainers.findOne({
-      where: { username: req.body.username, password: req.body.password },
-    });
-
-    if (trainerInfo != undefined) res.status(409).send('이미 존재하는 아이디입니다');
-    else {
-      const result = await trainers.create({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        age: req.body.age,
-        gender: req.body.gender,
-      });
-      res.status(200).json({ data: null, message: '회원가입을 환영합니다' });
-    }
-  } else {
-    res.status(400).json({ data: null, message: '모든 정보를 입력하세요' });
-  }
+//AWS s3 관련
+dotenv.config();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "ap-northeast-2",
 });
 
-// trainer login
-router.post('/login', async function (req, res) {
-  if (req.body.username && req.body.password) {
-    const trainerInfo = await trainers.findOne({
-      where: { username: req.body.username, password: req.body.password },
-    });
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "fitme-s3",
+        acl: "public-read",
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            cb(null, `${Date.now()}_${file.originalname}`);
+        },
+    }),
+});
 
-    if (trainerInfo != undefined) {
-      req.session.loggedin = true;
-      req.session.username = req.body.username;
-      res.redirect('/');
-      res.end();
-    } else {
-      res.status(401).json({ data: null, message: '로그인 정보가 일치하지 않습니다' });
+// trainer signup
+router.post(
+    "/signup",
+    upload.single("certificationFile"),
+    async function (req, res) {
+        console.log(req.body);
+
+        if (req.body.username && req.body.password) {
+            const trainerInfo = await trainers.findOne({
+                where: {
+                    username: req.body.username,
+                    password: req.body.password,
+                },
+            });
+
+            if (trainerInfo != undefined)
+                res.status(409).send("이미 존재하는 아이디입니다");
+            else {
+                try {
+                    const result = await trainers.create({
+                        username: req.body.username,
+                        password: req.body.password,
+                        email: req.body.email,
+                        age: req.body.age,
+                        gender: req.body.gender,
+                        introduction: req.body.introduction,
+                    });
+                } catch (err) {
+                    console.log(err);
+                }
+                res.status(200).json({
+                    data: null,
+                    message: "회원가입을 환영합니다",
+                });
+            }
+        } else {
+            res.status(400).json({
+                data: null,
+                message: "모든 정보를 입력하세요",
+            });
+        }
     }
-  } else {
-    res.status(400).json({ data: null, message: '아이디와 비밀번호를 입력하세요' });
-  }
+);
+
+// trainer login
+router.post("/login", async function (req, res) {
+    if (req.body.username && req.body.password) {
+        const trainerInfo = await trainers.findOne({
+            where: { username: req.body.username, password: req.body.password },
+        });
+
+        if (trainerInfo != undefined) {
+            req.session.loggedin = true;
+            req.session.username = req.body.username;
+            res.redirect("/");
+            res.end();
+        } else {
+            res.status(401).json({
+                data: null,
+                message: "로그인 정보가 일치하지 않습니다",
+            });
+        }
+    } else {
+        res.status(400).json({
+            data: null,
+            message: "아이디와 비밀번호를 입력하세요",
+        });
+    }
 });
 
 // trainers logout
-router.get('/logout', function (req, res) {
-  req.session.loggedin = false;
-  res.status(200).json({ data: null, message: '성공적으로 로그아웃되었습니다' });
+router.get("/logout", function (req, res) {
+    req.session.loggedin = false;
+    res.status(200).json({
+        data: null,
+        message: "성공적으로 로그아웃되었습니다",
+    });
 });
 
 // trainer delete
-router.post('/withdraw', async function (req, res) {
-  if (req.body.username && req.body.password) {
-    const trainerInfo = await trainers.findOne({
-      where: { username: req.body.username, password: req.body.password },
-    });
-    if (trainerInfo != undefined) {
-      await trainers.destroy({
-        where: { username: req.body.username, password: req.body.password },
-      });
-      res.status(200).json({ data: null, message: '성공적으로 탈퇴되었습니다' });
+router.post("/withdraw", async function (req, res) {
+    if (req.body.username && req.body.password) {
+        const trainerInfo = await trainers.findOne({
+            where: { username: req.body.username, password: req.body.password },
+        });
+        if (trainerInfo != undefined) {
+            await trainers.destroy({
+                where: {
+                    username: req.body.username,
+                    password: req.body.password,
+                },
+            });
+            res.status(200).json({
+                data: null,
+                message: "성공적으로 탈퇴되었습니다",
+            });
+        } else {
+            res.status(401).json({
+                data: null,
+                message: "아이디와 비밀번호를 확인하세요",
+            });
+        }
     } else {
-      res.status(401).json({ data: null, message: '아이디와 비밀번호를 확인하세요' });
+        res.status(400).json({
+            data: null,
+            message: "아이디와 비밀번호를 입력하세요",
+        });
     }
-  } else {
-    res.status(400).json({ data: null, message: '아이디와 비밀번호를 입력하세요' });
-  }
 });
 
 // trainer info
-router.get('/profile/:userid', async function (req, res) {
-  const trainerInfo = await trainers.findOne({
-    where: { username: req.params.userid },
-  });
-  res.status(200).json({ data: trainerInfo, message: '' });
+router.get("/profile/:userid", async function (req, res) {
+    const trainerInfo = await trainers.findOne({
+        where: { username: req.params.userid },
+    });
+    res.status(200).json({ data: trainerInfo, message: "" });
 });
 
 // trainer info change
-router.post('/profile/changeProfile/:userid', async function (req, res) {
-  if (req.session.loggedin) {
-    const trinersInfo = await trainers.findOne({
-      where: { username: req.params.userid },
-    });
-    if (req.body.password != req.body.password2)
-    res.status(401).json({ data: null, message: '입력된 비밀번호가 서로 다릅니다.' });
-  else {
-    await trainers.update(
-      {
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-        age: req.body.age,
-        gender: req.body.gender,
-      },
-      { where: { username: req.params.userid } }
-    );
-    res.status(200).json({ data: null, message: '성공적으로 변경되었습니다.' });
-  }
-  } else { 
-    res.status(401).json({ data: null, message: '로그인이 필요합니다.' });
-  }
+router.post("/profile/changeProfile/:userid", async function (req, res) {
+    if (req.session.loggedin) {
+        const trinersInfo = await trainers.findOne({
+            where: { username: req.params.userid },
+        });
+        if (req.body.password != req.body.password2)
+            res.status(401).json({
+                data: null,
+                message: "입력된 비밀번호가 서로 다릅니다.",
+            });
+        else {
+            await trainers.update(
+                {
+                    username: req.body.username,
+                    password: req.body.password,
+                    email: req.body.email,
+                    age: req.body.age,
+                    gender: req.body.gender,
+                },
+                { where: { username: req.params.userid } }
+            );
+            res.status(200).json({
+                data: null,
+                message: "성공적으로 변경되었습니다.",
+            });
+        }
+    } else {
+        res.status(401).json({ data: null, message: "로그인이 필요합니다." });
+    }
 });
 
-
-
 // trainerlist paging
-router.get('/trainerlist', async function (req, res) {
-  const trainerInfo = trainers.findAll({
-    attributes: ['name','age', 'gender', 'introduction']
-  });
-  res.send(trainerInfo);
+router.get("/trainerlist", async function (req, res) {
+    const trainerInfo = trainers.findAll({
+        attributes: ["name", "age", "gender", "introduction"],
+    });
+    res.send(trainerInfo);
 });
 
 // trainer search
-router.get('/trainerlist/${}', async function (req, res) {
+router.get("/trainerlist/${}", async function (req, res) {
     const trainerInfo = await trainers.findOne({
         where: { name: req.body.name },
     });
     if (trainerInfo != undefined) {
-      res.status(200).json({ data: trainerInfo, message: '' });
+        res.status(200).json({ data: trainerInfo, message: "" });
     } else {
-      res.status(200).json({ data: null, message: '검색결과가 없습니다' });
+        res.status(200).json({ data: null, message: "검색결과가 없습니다" });
     }
 });
 
 // trainer revenue
-router.get('/revenue', async function (req, res) {
-if (req.session.loggedin) {
-  const trainerInfo = await trainers.findOne({
-    where: { username: req.session.username },
-  });
-  res.status(200).json({ data: trainerInfo.point, message: '' });
-} else {
-  res.status(401).json({ data: null, message: '로그인이 필요합니다.' });
-}
+router.get("/revenue", async function (req, res) {
+    if (req.session.loggedin) {
+        const trainerInfo = await trainers.findOne({
+            where: { username: req.session.username },
+        });
+        res.status(200).json({ data: trainerInfo.point, message: "" });
+    } else {
+        res.status(401).json({ data: null, message: "로그인이 필요합니다." });
+    }
 });
-
 
 module.exports = router;
--- frontend/src/Page/Register/TrainerSignUpPage.js
+++ frontend/src/Page/Register/TrainerSignUpPage.js
@@ -1,166 +1,187 @@
-import React from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Container, Form, Button } from 'react-bootstrap';
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Container, Form, Button } from "react-bootstrap";
 
 function SignUpPage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [email, setEmail] = React.useState('');
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [passwordCheck, setPasswordCheck] = React.useState('');
-  const [gender, setGender] = React.useState('');
-  const [age, setAge] = React.useState('');
-  const [certificationFile, setCertificationFile] = React.useState(null);
-  const [introduction, setIntroduction] = React.useState('');
+    const [email, setEmail] = React.useState("");
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [passwordCheck, setPasswordCheck] = React.useState("");
+    const [gender, setGender] = React.useState("");
+    const [age, setAge] = React.useState("");
+    const [certificationFile, setCertificationFile] = React.useState(null);
+    const [introduction, setIntroduction] = React.useState("");
 
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-  const onChangeUsername = (e) => {
-    setUsername(e.target.value);
-  };
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
-  const onChangePasswordCheck = (e) => {
-    setPasswordCheck(e.target.value);
-  };
-  const onChangeGender = (e) => {
-    setGender(e.target.value);
-  };
-  const onChangeAge = (e) => {
-    setAge(e.target.value);
-  };
-  const onChangeCertificationFile = (e) => {
-    setCertificationFile(e.target.files[0]);
-  };
-  const onChangeIntroduction = (e) => {
-    setIntroduction(e.target.value);
-  };
-
-  // eslint-disable-next-line consistent-return
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (password !== passwordCheck) {
-      return setPasswordCheck('');
-    }
-
-    const body = {
-      email,
-      username,
-      password,
-      age,
-      gender,
-      certificationFile,
-      introduction,
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
     };
-    axios
-      .post('http://localhost:4000/trainers/signup', body)
-      .then((res) => {
-        if (res.data.success) {
-          navigate('/login');
-        } else {
-          alert(res.data.message);
+    const onChangeUsername = (e) => {
+        setUsername(e.target.value);
+    };
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const onChangePasswordCheck = (e) => {
+        setPasswordCheck(e.target.value);
+    };
+    const onChangeGender = (e) => {
+        setGender(e.target.value);
+    };
+    const onChangeAge = (e) => {
+        setAge(e.target.value);
+    };
+    const onChangeCertificationFile = (e) => {
+        setCertificationFile(e.target.files[0]);
+    };
+    const onChangeIntroduction = (e) => {
+        setIntroduction(e.target.value);
+    };
+
+    // eslint-disable-next-line consistent-return
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== passwordCheck) {
+            return setPasswordCheck("");
         }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
 
-  const goSignUp = () => {
-    navigate('/signup');
-  };
+        const body = {
+            email,
+            username,
+            password,
+            age,
+            gender,
+            introduction,
+        };
 
-  return (
-    <div className="signup">
-      <Container className="panel">
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>이메일</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={email}
-              onChange={onChangeEmail}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicUsername">
-            <Form.Label>이름</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter username"
-              value={username}
-              onChange={onChangeUsername}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>패스워드</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={onChangePassword}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicPasswordCheck">
-            <Form.Label>패스워드 재입력</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password Check"
-              value={passwordCheck}
-              onChange={onChangePasswordCheck}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicGender">
-            <Form.Label>성별</Form.Label>
-            <Form.Control as="select" onChange={onChangeGender}>
-              <option value="">선택하세요</option>
-              <option value="male">남성</option>
-              <option value="female">여성</option>
-              <option value="others">기타</option>
-            </Form.Control>
-          </Form.Group>
+        // axios
+        //   .post('http://localhost:4000/trainers/signup', body)
+        //   .then((res) => {
+        //     if (res.data.success) {
+        //       navigate('/login');
+        //     } else {
+        //       alert(res.data.message);
+        //     }
+        //   })
 
-          <Form.Group controlId="formBasicAge">
-            <Form.Label>나이</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter age"
-              value={age}
-              onChange={onChangeAge}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicCertificationFile">
-            <Form.Label>자격증 파일</Form.Label>
-            <Form.Control
-              type="file"
-              placeholder="Enter certificationFile"
-              onChange={onChangeCertificationFile}
-            />
-          </Form.Group>
-          <Form.Group controlId="formBasicIntroduction">
-            <Form.Label>간단한 자기소개를 입력해주세요</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder=""
-              value={introduction}
-              onChange={onChangeIntroduction}
-            />
-          </Form.Group>
+        const formData = new FormData();
+        formData.append("email", email);
+        formData.append("username", username);
+        formData.append("password", password);
+        formData.append("age", age);
+        formData.append("gender", gender);
+        formData.append("introduction", introduction);
+        formData.append("certificationFile", certificationFile);
 
-          <Button variant="primary" type="submit" onClick={onSubmit}>
-            Submit
-          </Button>
-          <Button variant="secondary" type="submit" onClick={goSignUp}>
-            일반 회원가입으로 이동
-          </Button>
-        </Form>
-      </Container>
-    </div>
-  );
+        axios({
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+            url: "http://localhost:4000/trainers/signup",
+            data: formData,
+            method: "POST",
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+
+    const goSignUp = () => {
+        navigate("/signup");
+    };
+
+    return (
+        <div className="signup">
+            <Container className="panel">
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>이메일</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            value={email}
+                            onChange={onChangeEmail}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>이름</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter username"
+                            value={username}
+                            onChange={onChangeUsername}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>패스워드</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={onChangePassword}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPasswordCheck">
+                        <Form.Label>패스워드 재입력</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password Check"
+                            value={passwordCheck}
+                            onChange={onChangePasswordCheck}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicGender">
+                        <Form.Label>성별</Form.Label>
+                        <Form.Control as="select" onChange={onChangeGender}>
+                            <option value="">선택하세요</option>
+                            <option value="male">남성</option>
+                            <option value="female">여성</option>
+                            <option value="others">기타</option>
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicAge">
+                        <Form.Label>나이</Form.Label>
+                        <Form.Control
+                            type="number"
+                            placeholder="Enter age"
+                            value={age}
+                            onChange={onChangeAge}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCertificationFile">
+                        <Form.Label>자격증 파일</Form.Label>
+                        <Form.Control
+                            type="file"
+                            placeholder="Enter certificationFile"
+                            onChange={onChangeCertificationFile}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicIntroduction">
+                        <Form.Label>간단한 자기소개를 입력해주세요</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            value={introduction}
+                            onChange={onChangeIntroduction}
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" onClick={onSubmit}>
+                        Submit
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        type="submit"
+                        onClick={goSignUp}
+                    >
+                        일반 회원가입으로 이동
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    );
 }
 
 export default SignUpPage;
