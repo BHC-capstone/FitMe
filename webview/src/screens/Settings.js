93cf32106307f30ead4c0f2b1931f1dbe7997602
pjm2476 <pjm2476@gmail.com>
2023-04-29 23:43:55 +0900

    android webview 구동 테스트

---
+++ webview/src/screens/Settings.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import WebView from 'react-native-webview';
+
+const Home = () => {
+  return <WebView source={{ uri: 'https://naver.com' }} />;
+};
+
+export default Home;
