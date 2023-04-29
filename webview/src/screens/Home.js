64810a2f0a83a69a39b844e0dc259ae3d37271f9
pjm2476 <pjm2476@gmail.com>
2023-04-29 20:24:25 +0900

    web view 재구성

--- webview/src/screens/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import WebView from 'react-native-webview';
-
-const Home = () => {
-  return <WebView source={{uri: 'https://naver.com'}} />;
-};
-
-export default Home;
