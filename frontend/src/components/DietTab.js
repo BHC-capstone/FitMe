a97f0b7d6a3bf9684f56d3e56f4f57cd7245e85e
parkbeomsu <poj4639@ajou.ac.kr>
2023-05-11 14:59:25 +0900

    diet tab update

--- frontend/src/components/DietTab.js
+++ frontend/src/components/DietTab.js
@@ -8,6 +8,9 @@ function DietTab({ userid, date }) {
   const imageInput2 = useRef();
   const imageInput3 = useRef();
   const [Dietdate, setDietdate] = useState([]);
+  const [ImageBreakfast, setImageBreakfast] = useState([]);
+  const [ImageLunch, setImageLunch] = useState([]);
+  const [ImageDinner, setImageDinner] = useState([]);
   /* 업로드 부분은 업로드 버튼을 누를때마다 데이터베이스 변경
     신청을 보낼지 아니면 제출 버튼을 하나 더만들어서 보내고 수정이
     불가능하게 할지 토론할 부분이 있어, 구현이 크게 오래걸리지 않는
@@ -28,26 +31,100 @@ function DietTab({ userid, date }) {
   const onCickImageUpload3 = () => {
     imageInput3.current.click();
   };
+  // 각각 입력받는 api가 3개가 되어야함
+  function onBreakfastChange(event) {
+    setImageBreakfast(event.target.files[0]);
+    const formData = new FormData();
+    formData.append('img', ImageBreakfast);
+    axios
+      .post('', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log('fail');
+      });
+    // console.log(event.target.files);
+  }
+  function onLunchChange(event) {
+    setImageLunch(event.target.files[0]);
+    const formData = new FormData();
+    formData.append('img', ImageLunch);
+    axios
+      .post('', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log('fail');
+      });
+    // console.log(event.target.files);
+  }
+  function onDinnerChange(event) {
+    setImageDinner(event.target.files[0]);
+    const formData = new FormData();
+    formData.append('img', ImageDinner);
+    axios
+      .post('', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log('fail');
+      });
+    // console.log(event.target.files);
+  }
+
   return (
     <Flexcontainers>
       <Flexcontainerg>
         <Textbox>● 아침: {Dietdate.breakfast}</Textbox>
         <Rightbox>
-          <input type="file" style={{ display: 'none' }} ref={imageInput1} />
+          <input
+            type="file"
+            style={{ display: 'none' }}
+            ref={imageInput1}
+            accept="image/png, image/jpeg, image/jpg"
+            onChange={event => onBreakfastChange(event)}
+          />
           <Button1 onClick={onCickImageUpload1}>이미지업로드</Button1>
         </Rightbox>
       </Flexcontainerg>
       <Flexcontainerg>
         <Textbox>● 점심: {Dietdate.lunch}</Textbox>
         <Rightbox>
-          <input type="file" style={{ display: 'none' }} ref={imageInput2} />
+          <input
+            type="file"
+            style={{ display: 'none' }}
+            ref={imageInput2}
+            accept="image/png, image/jpeg, image/jpg"
+            onChange={event => onLunchChange(event)}
+          />
           <Button1 onClick={onCickImageUpload2}>이미지업로드</Button1>
         </Rightbox>
       </Flexcontainerg>
       <Flexcontainerg>
         <Textbox>● 저녁: {Dietdate.dinner}</Textbox>
         <Rightbox>
-          <input type="file" style={{ display: 'none' }} ref={imageInput3} />
+          <input
+            type="file"
+            style={{ display: 'none' }}
+            ref={imageInput3}
+            accept="image/png, image/jpeg, image/jpg"
+            onChange={event => onDinnerChange(event)}
+          />
           <Button1 onClick={onCickImageUpload3}>이미지업로드</Button1>
         </Rightbox>
       </Flexcontainerg>
