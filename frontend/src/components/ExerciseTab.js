66bc229c3033721db3725ae4f3744b8cff3ef094
pjm2476 <pjm2476@gmail.com>
2023-05-14 18:59:33 +0900

    운동 루틴 고침

--- frontend/src/components/ExerciseTab.js
+++ frontend/src/components/ExerciseTab.js
@@ -1,11 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Container, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Routine from './Routine';
+
 // eslint-disable-next-line react/prop-types
 function ExerciseTab({ userid, date }) {
   const [exerdate, setExerdate] = useState([]);
+  // {index === currentTab ? 'submenu focused' : 'submenu'}
   const navigate = useNavigate();
   // 해당 링크로 이동, 여기서는
   const navigateToRequest = ({ link }) => {
@@ -22,20 +25,18 @@ function ExerciseTab({ userid, date }) {
   // 운동 루틴이 배열로 제공 된다고 가정하면 map 함수를 상위에 추가하여 밑의 컴포넌트들을 본문으로 사용할 예정
   return (
     <Flexcontainers>
-      <Container>
-        <Flexcontainerg>
-          <Textbox>● 루틴 1: {exerdate.routine}</Textbox>
-          <Flexcontainerg>
-            <Button1 size="large" onClick={navigateToRequest}>
-              해당 운동 루틴 영상
-            </Button1>
-            <Button1 size="large" onClick={navigateToRequest}>
-              촬영 가이드
-            </Button1>
-            <Button1 size="large">운동 영상 업로드</Button1>
-          </Flexcontainerg>
-        </Flexcontainerg>
-      </Container>
+      <Routine />
+      {exerdate.map((el, index) => (
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+        <Routine
+          num={index}
+          exercisename={el.exercisename}
+          time={el.time}
+          set={el.set}
+          exerciseURL={el.exerciseURL}
+          guideURL={el.guideURL}
+        />
+      ))}
     </Flexcontainers>
   );
 }
@@ -43,26 +44,5 @@ const Flexcontainers = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-`;
-const Flexcontainerg = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-const Textbox = styled.div`
-  float: left;
-  font-weight: 400;
-  font-size: 16px;
-`;
-const Button1 = styled(Button)`
-  width: 120px;
-  float: right;
-  margin-right: 100px;
-  margin-top: 0px;
-  vertical-align: top;
-  //border: 3px solid white;
-`;
-const Rightbox = styled.div`
-  float: right;
 `;
 export default ExerciseTab;
