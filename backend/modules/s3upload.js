503f514e366af21b494c0ba15198ad30e656fd75
noyouhyun <84374791+noyouhyun@users.noreply.github.com>
2023-05-16 23:13:41 +0900

    유저 운동 영상 업로드 api 작성

--- backend/modules/s3upload.js
+++ backend/modules/s3upload.js
@@ -5,11 +5,29 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const upload = multer({ storage: multer.memoryStorage() });
+
+const videoupload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 50 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('video/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('비디오만 업로드 가능합니다.'));
+    }
+  },
+});
+
+
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: 'ap-northeast-2',
 });
+
+
+
+
 
 module.exports = {
   upload,
--- backend/routes/calender.js
+++ backend/routes/calender.js
@@ -10,6 +10,7 @@ const AWS = require('aws-sdk');
 
 //AWS s3 관련
 const imageUpload = require('../modules/s3upload').upload;
+const videoupload = require('../modules/s3upload').videoupload;
 const s3 = require('../modules/s3upload').s3;
 
 dotenv.config();
@@ -164,57 +165,48 @@ router.post(
 );
 
 // upload exerciseroutine video
-router.post(
-  '/exercisevideo/:id',
-  imageUpload.fields([{ name: 'video', maxCount: 1 }]),
-  async (req, res) => {
+router.post('/exercisevideo/:id/:routineid',videoUpload.single('video'),async (req, res) => {
     if (req.session.loggedin) {
-    try {
-      const { id, date } = req.params;
-      const exerciseRoutineDate = await schedules.findOne({
-        where: { user_id: id, date: date },
-        attributes: ['routine_id'],
-      });
-
-      if (!exerciseRoutineDate) {
-        return res.status(400).json({
-          data: null,
-          message: '해당 날짜의 운동루틴이 존재하지 않습니다.',
+      try {
+        const { routineid, id } = req.params;
+        const exerciseRoutine = await exercise_routines.findOne({
+          where: { id: routineid },
         });
-      }
 
-      const exerciseRoutine = await exercise_routine.findOne({
-        where: { id: exerciseRoutineDate.routine_id },
-      });
-
-      if (!exerciseRoutine) {
-        return res
-          .status(400)
-          .json({ data: null, message: '해당 운동루틴이 존재하지 않습니다.' });
-      }
-
-      const exerciseRoutineId = exerciseRoutine.id;
-      const videoUrl = req.files.video ? req.files.video[0].path : null;
-
-      await exercise_routine.update(
-        {
-          user_video_url: videoUrl,
-        },
-        {
-          where: { id: exerciseRoutineId },
+        if (!exerciseRoutine) {
+          return res.status(400).json({ data: null, message: '해당 운동 루틴이 존재하지 않습니다.' });
         }
-      );
 
-      res
-        .status(200)
-        .json({ data: null, message: '운동 영상 업로드가 완료되었습니다.' });
-    } catch (err) {
-      console.log(err);
+        const uploadParams = {
+          acl: 'public-read',
+          ContentType: req.file.mimetype,
+          Bucket: 'fitme-s3',
+          Body: req.file.buffer,
+          Key: `exerciseroutine/${id}/${routineid}/${req.file.originalname}`,
+        };
+
+        const result = await s3.upload(uploadParams).promise();
+
+        await exercise_routines.update(
+          {
+            user_video_url: result.Location,
+          },
+          {
+            where: { id: routineid },
+          }
+        );
+
+        res.status(200).json({ data: null, message: '운동영상 업로드가 완료되었습니다.' });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ data: null, message: '서버 오류가 발생했습니다.' });
+      }
+    } else {
+      res.status(401).json({ data: null, message: '로그인이 필요합니다.' });
     }
-           } else {
-               res.status(401).json({ data: null, message: '로그인이 필요합니다.' });
-           }
   }
 );
+
+
 
 module.exports = router;
