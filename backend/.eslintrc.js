73fbb3b86fde2a987e1451bcf3dc41c196fc3f3c
noyouhyun <84374791+noyouhyun@users.noreply.github.com>
2023-05-27 05:11:21 +0900

    영상 관련 삭제 및 수정 api 추가

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,19 +5,39 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
-    'airbnb/hooks'
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'react-app',
+    'prettier',
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 14,
-  },
-  plugins: [
-    'react',
-  ],
+  plugins: ['prettier'],
   rules: {
+    'react/prop-types': 'off',
+    'import/np-resolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/jsx-no-bind': [
+      1,
+      {
+        allowArrowFunctions: true,
+        allowFunctions: true,
+        allowBind: true,
+      },
+    ],
   },
 };
