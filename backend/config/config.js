bb15ac28c155c80e30810b9464af13165b65f875
pjm2476 <pjm2476@gmail.com>
2023-06-03 20:45:50 +0900

    패키지락 업데이트

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,25 +3,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    development: {
-        username: 'root',
-        password: '1234',
-        database: 'FitMe',
-        host: '127.0.0.1',
-        dialect: 'mysql'
-    },
-    test: {
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        host: process.env.MYSQL_HOST,
-        dialect: 'mysql'
-    },
-    production: {
-        username: 'root',
-        password: '1234',
-        database: 'FitMe',
-        host: '127.0.0.1',
-        dialect: 'mysql'
-    }
+  development: {
+    username: 'root',
+    password: '1234',
+    database: 'FitMe',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: '1234',
+    database: 'FitMe',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
 };
