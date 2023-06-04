ef24b54ca71baf22918bda68fcb0509418682322
pjm2476 <pjm2476@gmail.com>
2023-06-04 19:32:52 +0900

    deploy.sh 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,12 @@
 
 #!/bin/bash
 
-# 필요한 작업을 수행하는 스크립트 내용을 작성합니다.
-
-# 예시: 필드 파일을 S3에서 다운로드하여 애플리케이션 디렉토리로 이동합니다.
 aws s3 cp s3://fitme-s3/practice-deploy.zip /home/ubuntu/action/ --recursive
 
 cd /home/ubuntu/action/
-unzip practice-deploy.zip -d ~/home/ubuntu/deploy
+unzip practice-deploy.zip -d /home/ubuntu/deploy
 rm practice-deploy.zip
 
-
-#
 cd ~/home/ubuntu/deploy/FitMe/backend
 
 if pgrep -f "npm start" >/dev/null; then
