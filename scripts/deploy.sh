c17b73a9177558440dd2c7edec330cde06a6eab5
pjm2476 <pjm2476@gmail.com>
2023-06-04 19:57:34 +0900

    code deploy 테스트

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,22 +3,23 @@
 
 aws s3 cp s3://fitme-s3/practice-deploy.zip /home/ubuntu/action/ --recursive
 
-# cd /home/ubuntu/action/
+cd /home/ubuntu/action/
+cp /home/ubuntu/action /home/ubuntu/deploy
 # unzip practice-deploy.zip -d /home/ubuntu/deploy
 # rm practice-deploy.zip
 
-# cd ~/home/ubuntu/deploy/FitMe/backend
+cd /home/ubuntu/deploy/FitMe/backend
 
-# if pgrep -f "npm start" >/dev/null; then
-#   # 실행 중인 경우 종료
-#   pkill -f "npm start"
-#   echo "Application stopped."
-#   npm start
-# else
-#   # 실행 중이지 않은 경우 시작
-#   npm start
-#   echo "Application started."
-# fi
-# echo "Deployment completed."
+if pgrep -f "npm start" >/dev/null; then
+  # 실행 중인 경우 종료
+  pkill -f "npm start"
+  echo "Application stopped."
+  npm start
+else
+  # 실행 중이지 않은 경우 시작
+  npm start
+  echo "Application started."
+fi
+echo "Deployment completed."
 
-# exit 0
+exit 0
