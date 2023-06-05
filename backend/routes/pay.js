7477505c1886b4d9a447e46282a2eb31403a6b23
noyouhyun <84374791+noyouhyun@users.noreply.github.com>
2023-06-05 19:16:56 +0900

    관리자 및 각종 api 추가 및 수정

--- backend/routes/pay.js
+++ backend/routes/pay.js
@@ -2,13 +2,13 @@ var express = require("express");
 var router = express.Router();
 const { sequelize } = require("../models");
 const {
-    trainers,
-    users,
-    pt_requests,
-    exercise_routines,
-    schedules,
-    trainer_manage,
-    meal_plan,
+  trainers,
+  users,
+  pt_requests,
+  exercise_routines,
+  schedules,
+  trainer_manage,
+  meal_plan,
 } = require("../models");
 const initModels = require("../models/init-models");
 const models = initModels(sequelize);
@@ -16,46 +16,47 @@ const dotenv = require("dotenv");
 const multer = require("multer");
 const AWS = require("aws-sdk");
 const { mod } = require("prelude-ls");
-
-//AWS s3 관련
-const imageUpload = require("../modules/s3upload").upload;
-const videoupload = require("../modules/s3upload").videoUpload;
-const s3 = require("../modules/s3upload").s3;
-
+const axios = require("axios");
 dotenv.config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);  // Stripe API 키
 
-// 결제 API 엔드포인트
-router.post('/payment', async (req, res) => {
-  const { amount, currency, source, description } = req.body;
+router.post("/approval", async (req, res) => {
+  const { cid, tid, partner_order_id, partner_user_id, pg_token } = req.body;
+
+  const url = "https://kapi.kakao.com/v1/payment/approve";
+  const apiKey = "YOUR_KAKAOPAY_API_KEY";
 
   try {
-    // Stripe 결제 요청
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency,
-      source,
-      description,
-    });
+    const response = await axios.post(
+      url,
+      {
+        cid,
+        tid,
+        partner_order_id,
+        partner_user_id,
+        pg_token,
+      },
+      {
+        headers: {
+          Authorization: `KakaoAK ${apiKey}`,
+        },
+      }
+    );
 
-    // 결제 성공
-    res.status(200).json({
-      success: true,
-      paymentIntentId: paymentIntent.id,
-      message: 'Payment successful',
-    });
+    if (response.data.code === 0) {
+      res
+        .status(200)
+        .json({ success: true, message: "Payment approved successfully" });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "Payment approval failed" });
+    }
   } catch (error) {
-    // 결제 실패
-    console.error(error);
     res.status(500).json({
       success: false,
-      message: 'Payment failed',
+      message: "Error occurred during payment approval",
     });
   }
 });
-
-module.exports = router;
-
-
 
 module.exports = router;
